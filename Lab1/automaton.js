ca9711483cc6a77e92116ef19b8ff414cc287655
Michael Kane <michaelpatrickgilliskane@gmail.com>
2017-09-23 16:19:21 +0100

    Adding comments for automaton.

--- Lab1/automaton.js
+++ Lab1/automaton.js
@@ -1,13 +1,16 @@
+//Rules for active and inactive cells.
 function rules(left, middle, right) {
-	//activate.
+	//active.
 	if(left==1 && middle==1 && right==1 || left==1 && middle==0 && right==1 || left==1 && middle==0 && right==0 || left==0 && middle==1 && right==0 || left==0 && middle==0 && right==1) {
 		return activity(1)
 	}
+	//inactive.
 	else if(left==1 && middle==1 && right==0 || left==0 && middle==1 && right==1 || left==0 && middle==0 && right==0) {
 		return activity(0)
 	}
 }
 
+//Colours a cell black if inactive and colours a cell red if active.
 function activity(num) {
 	//inactive
 	if(num == 0) {
@@ -20,6 +23,7 @@ function activity(num) {
 	return num;
 }
 
+//Generates a 101*50 grid of cells(black=inactive red = active).
 function automaton(){
 	var parentRow = [];
 	var childRow = [];
@@ -32,7 +36,7 @@ function automaton(){
 				randomNumber = Math.round(Math.random());
 				childRow = childRow+activity(randomNumber);
 			}
-			//generate the next 49 rows based on the rules below.
+			//generate the next 49 rows based on rules().
 			else{
 				//check if its the first cell.
 				if(j == 0){
@@ -42,12 +46,13 @@ function automaton(){
 				else if(j == 100){
 					childRow[j] = activity(parentRow[0]);
 				}
+				//Generate the cells which are not the first or last cell.
 				else{
 					childRow[j] = rules(parentRow[j-1],parentRow[j],parentRow[j+1])
 				}
-				
 			}
 		}
+		//The childRow becomes the new ParentRow and the childRow is reset.
 		parentRow = childRow;
 		childRow = [];
 	}
