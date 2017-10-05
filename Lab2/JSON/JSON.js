7fbc2626f818f8cda56148f91b06304792c1d7f4
Michaelkane428 <michaelkane428@gmail.com>
2017-10-06 00:49:53 +0100

    Finished questions 2 :)

--- Lab2/JSON/JSON.js
+++ Lab2/JSON/JSON.js
@@ -1,8 +1,13 @@
 let usernames = [];
 let geo_objects = [];
+let userdetails = [];
+let streetnames = [];
+let userids = [];
 
 let formatedUserNames = [];
 let formattedGeoOjects = [];
+let formatteduserdetails = [];
+let formattedstreetnames = [];
 
 // https://stackoverflow.com/questions/2499567/how-to-make-a-json-call-to-a-url?noredirect=1&lq=1
 function getjson(){
@@ -14,7 +19,7 @@ function getjson(){
     Httpreq.send(null);
     return Httpreq.responseText;          
   }
-  
+
   let json_obj = JSON.parse(Get(url));
   let length = json_obj.length;
   
@@ -23,11 +28,32 @@ function getjson(){
     geo_objects[i] = JSON.stringify(json_obj[i].address.geo);
     formatedUserNames[i] = "<br><li>" + usernames[i];
     formattedGeoOjects[i] = "<br><li>" + geo_objects[i];
-    
-    
   }
   document.getElementById("usernames").innerHTML = "List of usernames: " + formatedUserNames;
   document.getElementById("geo").innerHTML = "List of Geo Objects: " + formattedGeoOjects;
+  
+  for(i=0; i< length; i++){
+    if(json_obj[i].address.zipcode.charAt(0) == 5){
+      streetnames[i] = json_obj[i].address.street;
+      formattedstreetnames[i] = "<br><li>" + streetnames[i];
+    }
+    userdetails[i] = [json_obj[i].name, json_obj[i].id, json_obj[i].company.name, json_obj[i].address.zipcode];
+    formatteduserdetails[i] = "<br><li>" + userdetails[i];
+  }
+  document.getElementById("userdetails").innerHTML = "List of User Details: " + formatteduserdetails;
+  document.getElementById("streetnames").innerHTML = "List of Street Names: " + formattedstreetnames;
+  
+  for(i=0; i< length; i++){
+    if(i != length-1){
+        userids[i] = json_obj[i].id + "*";
+    }
+    else{
+      userids[i] = json_obj[i].id
+    }
+  }
+  product = userids.toString().replace(/,/g,'');
+  document.getElementById("product").innerHTML = "Product of Ids: " +eval(product)
+  
 }
 
 getjson();
