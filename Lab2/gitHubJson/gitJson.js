fc8b8e81a4507f3a8e03dcde7ca77e0702aa6b4a
Michaelkane428 <michaelkane428@gmail.com>
2017-10-15 22:27:43 +0100

    Finished Lab 2

--- Lab2/gitHubJson/gitJson.js
+++ Lab2/gitHubJson/gitJson.js
@@ -1,7 +1,9 @@
-const Git = (Json) =>{
-	Json.addEventListener("click", on_click);
-    input = Json.querySelector("#userInput");
+const Git = (Search, UserRepos) =>{
+	Search.addEventListener("click", on_click);
+    UserRepos.addEventListener("click", on_click);
 	let url = 'https://api.github.com/users';
+    let login ="";
+    generateTextArea(url);
     //https://stackoverflow.com/questions/23835150/javascript-event-listener-for-multiple-buttons-with-same-class-name
 	// add an event listener for ALL BUTTONS
 	function on_click() {
@@ -11,16 +13,21 @@ const Git = (Json) =>{
       let element = event.target;
       while (element) {
         if (element.nodeName === "BUTTON" && /searchButton/.test(element.className)) {
-          getLogins(url);
+          login = document.getElementById("userInput").value;
+          getLogins(url+ '/' + login);
 		  break;
           }
+        else if(element.nodeName === "TEXTAREA" && /displayUserRepoInfo/.test(element.className)){
+          login = element.innerHTML;
+          getLogins(url+ '/' + login);
+		  break;
+        }
 
       element = element.parentNode;
 	  }
 	}
-  
-	function getLogins(url){
-        url = url + input.value; 
+    
+    function generateTextArea(url){
 		var Httpreq = new XMLHttpRequest(); // a new request
 		Httpreq.open("GET",url,true);
 		Httpreq.reponseType = "json";
@@ -29,15 +36,40 @@ const Git = (Json) =>{
 			if (Httpreq.readyState==4 && Httpreq.status==200) {
 				let json_obj = JSON.parse(Httpreq.responseText);
 				let length = json_obj.length;
-				console.log(json_obj);
-				for(i=0; i< length; i++){
-				  if(document.getElementById("UserInput").innerHTML == json_obj[i].name){
-					console.log("YES");
-				  }
-				}
+              for(i=0; i<length;i++){
+                if(length > 5){
+                  document.getElementById('UserRepos').style.overflow ="scroll";
+                  document.getElementById('UserRepos').innerHTML += '<textarea class="displayUserRepoInfo" type="text" readonly="readonly">' +  json_obj[i].login +'</textarea>';
+                }
+                else{
+                  document.getElementById('UserRepos').innerHTML += '<textarea class="displayUserRepoInfo" type="text" readonly="readonly">' + json_obj[i].login +'</textarea>'
+                }
+             
+                
+              }
+			}
+		}
+		Httpreq.send(null);
+	}
+  
+	function getLogins(url){
+		var Httpreq = new XMLHttpRequest(); // a new request
+		Httpreq.open("GET",url,true);
+		Httpreq.reponseType = "json";
+		// Wait for the response to finish. Then execute the following code
+		Httpreq.onreadystatechange = function() {
+			if (Httpreq.readyState==4 && Httpreq.status==200) {
+				let json_obj = JSON.parse(Httpreq.responseText);
+				let length = json_obj.length;
+                document.getElementById("img").src = json_obj.avatar_url;
+                document.getElementById("Name").value = json_obj.name;
+                document.getElementById("UserName").value = json_obj.login;
+                document.getElementById("Email").value = json_obj.email;
+                document.getElementById("Location").value = json_obj.location;
+                document.getElementById("Gist").value = json_obj.public_gists;
 			}
 		}
 		Httpreq.send(null);
 	}
 }
-Git(document.getElementById('Search'));
+Git(document.getElementById('Search'), document.getElementById('UserRepos'));
